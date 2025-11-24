ad98f9973a1996622c875ccc890f48a360c6fa9d
Sutan Mufti <sutan@enums.id>
2025-11-24 16:15:53 +0700

    nojekyll and no trailing slash

--- _app/immutable/chunks/BtoqNkRU.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_drqh75)==null?void 0:s.base)??"/gagasceria-landingpage";var a;const t=((a=globalThis.__sveltekit_drqh75)==null?void 0:a.assets)??e??"";export{t as a,e as b};
